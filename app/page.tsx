fc5a8d2e1e8f4d541e662907af83837bbaee25b5
Jordan Epstein <jorepstein1@gmail.com>
2025-07-25 12:22:11 -0400

    Add Spotify sign-in button

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,30 @@
 import Image from "next/image";
+import { auth, signIn, signOut } from "@/auth";
 
-export default function Home() {
+function SignIn({
+  provider,
+  ...props
+}: { provider?: string } & React.ComponentPropsWithRef<"button">) {
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signIn(provider);
+      }}
+    >
+      <button {...props}>Sign In</button>
+    </form>
+  );
+}
+
+export default async function Home() {
+  const session = await auth();
+  if (!session?.user)
+    return (
+      <div>
+        <SignIn />
+      </div>
+    );
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
